c0cbefc8f99c7b745612a63102306ada5994bfdf
bpmb82 <boudewijnbeckers@gmail.com>
2024-12-11 11:10:45 +0100

    Added BitbucketCloudWebhookReceiver to support Bitbucket Cloud webhooks

--- awx/api/urls/webhooks.py
+++ awx/api/urls/webhooks.py
@@ -1,6 +1,6 @@
 from django.urls import re_path
 
-from awx.api.views.webhooks import WebhookKeyView, GithubWebhookReceiver, GitlabWebhookReceiver, BitbucketDcWebhookReceiver
+from awx.api.views.webhooks import WebhookKeyView, GithubWebhookReceiver, GitlabWebhookReceiver, BitbucketDcWebhookReceiver, BitbucketCloudWebhookReceiver
 
 
 urlpatterns = [
@@ -8,4 +8,5 @@ urlpatterns = [
     re_path(r'^github/$', GithubWebhookReceiver.as_view(), name='webhook_receiver_github'),
     re_path(r'^gitlab/$', GitlabWebhookReceiver.as_view(), name='webhook_receiver_gitlab'),
     re_path(r'^bitbucket_dc/$', BitbucketDcWebhookReceiver.as_view(), name='webhook_receiver_bitbucket_dc'),
+    re_path(r'^bitbucket_cloud/$', BitbucketCloudWebhookReceiver.as_view(), name='webhook_receiver_bitbucket_cloud'),
 ]
